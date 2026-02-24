17262785292dad72396884ccee6e081716776e38
SallahBoussettah <boussettahsallah@gmail.com>
2026-02-24 04:09:51 +0000

    Add if/else conditions and first custom function with parameters

--- TheMovingCube/Assets/Scripts/HelloWorld.cs
+++ TheMovingCube/Assets/Scripts/HelloWorld.cs
@@ -4,10 +4,23 @@ public class HelloWorld : MonoBehaviour
 {
     void Start()
     {
-        float numA = 5.5f;
-        int numB = 10;
-        // int sum = (int)numA + numB;
-        float sum = numA + numB;
-        Debug.Log("Hello, Salah! The sum of " + numA + " and " + numB + " is: " + sum);
+        int playerHealth = 49;
+        CheckHealth(playerHealth);
+        CheckHealth(0);
+        CheckHealth(60);
+    }
+
+    void CheckHealth(int health)
+    {
+        if (health <= 0) 
+        { 
+            Debug.Log("Player Dead");
+        } else if (health < 50)
+        {
+            Debug.Log("Player health is half");
+        } else
+        {
+            Debug.Log("Player Alive");
+        }
     }
 }
