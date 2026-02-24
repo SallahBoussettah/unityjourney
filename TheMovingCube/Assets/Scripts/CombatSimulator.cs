4dbd2091035994f3331b25af8f9296c1989b56ec
SallahBoussettah <boussettahsallah@gmail.com>
2026-02-24 07:23:52 +0000

    WASD movement with Q/E rotation and shift speed boost
    - Speed multiplier variable for shift boost on both move and rotate
    - GetKeyDown for one-time shift press log
    - Q/E rotation with intentional else-if (opposite directions)

--- TheMovingCube/Assets/Scripts/CombatSimulator.cs
+++ TheMovingCube/Assets/Scripts/CombatSimulator.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class CombatSimulator : MonoBehaviour
 {
     public float moveSpeed = 10f;
+    public float rotateSpeed = 50f;
+    public int speedMultiplier = 2;
     void Start()
     {
         
@@ -10,18 +12,38 @@ public class CombatSimulator : MonoBehaviour
 
     void Update()
     {
+        float currentSpeed = moveSpeed; 
+        float currentRotation = rotateSpeed;
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            currentSpeed = moveSpeed * speedMultiplier;
+            currentRotation = rotateSpeed * speedMultiplier;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Debug.Log("LeftShift Pressed");
+        }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+            transform.Translate(0, 0, currentSpeed * Time.deltaTime);
         } if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, 0, -(moveSpeed * Time.deltaTime));
+            transform.Translate(0, 0, -(currentSpeed * Time.deltaTime));
         } if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(-(moveSpeed * Time.deltaTime), 0, 0);
+            transform.Translate(-(currentSpeed * Time.deltaTime), 0, 0);
         } if (Input.GetKey(KeyCode.D))
         {
-            transform.Translate(moveSpeed * Time.deltaTime, 0, 0);
+            transform.Translate(currentSpeed * Time.deltaTime, 0, 0);
+        }
+
+        // Improvising and adding rotate on E and Q, with Left Shift Boost
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, currentRotation * Time.deltaTime, 0);
+        } else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -(currentRotation * Time.deltaTime), 0);
         }
     }
 
